98ccddf27bbe254dcf99f3a47d4b9aa70e288e15
YUNUS EMRE AKCA <2022212026@cumhuriyet.edu.tr>
2023-05-23 21:40:44 +0300

    10.gün ödev 1

--- Business/Abstract/ICarService.cs
+++ Business/Abstract/ICarService.cs
@@ -1,4 +1,5 @@
-﻿using Entities.Concrete;
+﻿using Core.Utilities.Result;
+using Entities.Concrete;
 using Entities.DTOs;
 using System;
 using System.Collections.Generic;
@@ -10,13 +11,13 @@ namespace Business.Abstract
 {
     public interface ICarService
     {
-        List<Car> GetAll();
-        List<Car> GetCarsByBrandId(int id);
-        List<Car> GetCarsByColorId(int id);
-        List<CarDetailDto> GetCarDetails();
-        Car GetById(int carId);
-        void Delete(Car car);
-        void Update(Car car);
-        void Add(Car car);
+        IDataResult<List<Car>> GetAll();
+        IDataResult<List<Car>> GetCarsByBrandId(int id);
+        IDataResult<List<Car>> GetCarsByColorId(int id);
+        IDataResult<List<CarDetailDto>> GetCarDetails();
+        IDataResult<Car> GetById(int carId);
+        IResult Delete(Car car);
+        IResult Update(Car car);
+        IResult Add(Car car);
     }
 }
---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Constants
+{
+    public class Messages
+    {
+        public static string MaintenanceTime = "Bakım zamanı";
+        public static string CarAdded = "Araba eklendi";
+        public static string CarNameInvalid = "Araba ismi geçersiz";
+        public static string CarPriceInvalid = "Araba fiyatı geçersiz";
+        public static string CarListed = "Araba listelendi";
+        public static string CarUpdated = "Araba güncellendi";
+        public static string CarDeleted = "Araba Silindi";
+    }
+}
