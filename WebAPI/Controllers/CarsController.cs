efcde018983d660585a5bfc1a08f8e63a464e281
YUNUS EMRE AKCA <2022212026@cumhuriyet.edu.tr>
2023-06-28 21:04:01 +0300

    hataların düzeltilmesi

--- WebAPI/Controllers/CarsController.cs
+++ WebAPI/Controllers/CarsController.cs
@@ -33,7 +33,7 @@ namespace WebAPI.Controllers
             var result = _carService.GetById(carId);
             if (result.Success)
             {
-                return Ok(result.Data.CarId);
+                return Ok(result);
             }
             return BadRequest(result);
         }
